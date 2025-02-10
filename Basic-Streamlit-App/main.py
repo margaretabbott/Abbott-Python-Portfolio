1a9eda747e166564c91ef30b7091a7c14bc6003b
Margaret Abbott <margaretcabbott@gmail.com>
2025-02-09 19:41:58 -0500

    update

--- Basic-Streamlit-App/main.py
+++ Basic-Streamlit-App/main.py
@@ -1,2 +1,9 @@
 import streamlit as st
+import pandas as pd
 st.title("Hello, Streamlit")
+st.write("Streamlit lets me build interactive web apps.")
+penguins=pd.read_csv("data/my_data_penguins.csv")
+st.dataframe(penguins)
+island=st.selectbox("Select an island",penguins["island"].unique())
+filtered_penguins=penguins[penguins["island"]==island]
+st.dataframe(filtered_penguins)
